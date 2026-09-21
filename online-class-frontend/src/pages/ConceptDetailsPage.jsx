7e731fb1a69cb41cc3d5f8aa334a1c438931a567
onlineclasssupport-lang <onlineclasssupport@gmail.com>
2026-09-21 11:45:27 +0530

    Update ConceptDetailsPage.jsx

--- online-class-frontend/src/pages/ConceptDetailsPage.jsx
+++ online-class-frontend/src/pages/ConceptDetailsPage.jsx
@@ -421,7 +421,7 @@ export default function ConceptDetailsPage() {
       <PageBackgroundLogo
         variant="global"
         className="oc-curriculum-bg-logo"
-        opacity={0.16}
+        opacity={0.40}
         size="min(860px, 88vw)"
       />
 
